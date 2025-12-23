e10f1bda8f3e379277b71a5b77a6df3c2b7c6ad4
Vic-117 <>
2025-12-22 18:18:13 -0600

    BD

---
+++ src/main/java/vPerez/ProgramacionNCapasNov2025/RestController/UsuarioRestController.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vPerez.ProgramacionNCapasNov2025.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vPerez.ProgramacionNCapasNov2025.DAO.DireccionJpaDAOImplementation;
+import vPerez.ProgramacionNCapasNov2025.DAO.PaisJpaDAOImplementation;
+import vPerez.ProgramacionNCapasNov2025.DAO.UsuarioJpaDAOImplementation;
+import vPerez.ProgramacionNCapasNov2025.JPA.Direccion;
+import vPerez.ProgramacionNCapasNov2025.JPA.Result;
+import vPerez.ProgramacionNCapasNov2025.JPA.Usuario;
+
+/**
+ *
+ * @author digis
+ */
+@RestController
+@RequestMapping("api/usuarios")
+public class UsuarioRestController {
+
+    @Autowired
+    UsuarioJpaDAOImplementation usuarioJpaDaoImplementation;
+    
+    @Autowired
+    DireccionJpaDAOImplementation direccionJpaDaoImplementation; 
+    
+    @Autowired
+    PaisJpaDAOImplementation paisJpaDaoImplementation;
+
+    @GetMapping
+    public ResponseEntity getAll() {
+        Result result = usuarioJpaDaoImplementation.getAll();
+        return ResponseEntity.status(result.StatusCode).body(result.Object);
+    }
+
+    @GetMapping("{idUsuario}")
+    public ResponseEntity getById(@PathVariable("idUsuario") int idUsuario) {
+        Result result = usuarioJpaDaoImplementation.getDireccionUsuarioById(idUsuario);
+
+        return ResponseEntity.status(result.StatusCode).body(result.Object);
+
+    }
+
+    @PostMapping
+    public ResponseEntity addUsuario(@RequestBody Usuario body) {
+        Result result = usuarioJpaDaoImplementation.add(body);
+
+        return ResponseEntity.status(result.StatusCode).build();
+    }
+    
+    @PutMapping("{idUsuario}")
+    public ResponseEntity updateUsuario(@RequestBody Usuario usuarioBody){
+        Result result = usuarioJpaDaoImplementation.update(usuarioBody);
+        
+        return ResponseEntity.status(result.StatusCode).build();
+        
+    }
+    
+    @DeleteMapping("{idUsuario}")
+    public ResponseEntity deleteUsuario(@PathVariable("idUsuario") int idUsuario){
+        Result result = usuarioJpaDaoImplementation.delete(idUsuario);
+        return ResponseEntity.status(result.StatusCode).build();
+    }
+    
+    @PostMapping("/busqueda")
+    public ResponseEntity busquedaAbierta(@RequestBody Usuario usuarioBody){
+        Result result = usuarioJpaDaoImplementation.GetAllDinamico(usuarioBody);
+        
+        return ResponseEntity.status(result.StatusCode).body(result.Objects);
+    }
+    
+//    @PatchMapping("{idUsuario}")
+//    public ResponseEntity bajaLogica(@PathVariable("idUsuario") int idUsuario, @RequestBody Usuario usuarioBody){
+//        usuarioBody.setIdUsuario(idUsuario);
+//        Result result = usuarioJpaDaoImplementation.softDelete(usuarioBody);
+//        return ResponseEntity.status(result.StatusCode).build();
+//    }
+    
+    @PatchMapping
+    public ResponseEntity bajaLogica(@RequestBody Usuario usuarioBody) {
+        Result result = usuarioJpaDaoImplementation.softDelete(usuarioBody);
+        return ResponseEntity.status(result.StatusCode).build();
+    }
+    
+    @PostMapping("direccion/agregar/{idUsuario}")
+    public ResponseEntity direccionAdd(@PathVariable("idUsuario") int idUsuario, @RequestBody Direccion direccionBody){
+        Result result = direccionJpaDaoImplementation.add(direccionBody, idUsuario);
+        return ResponseEntity.status(result.StatusCode).build();
+    }
+    
+    @PutMapping("direccion/editar")
+    public ResponseEntity direccionUpdate( @RequestBody Usuario usuarioBody){
+//        direccionBody.setIdDireccion(idDireccion);
+//    usuarioBody.direcciones.add(new Direccion());
+        Result result = direccionJpaDaoImplementation.update(usuarioBody.direcciones.get(0));
+        return ResponseEntity.status(result.StatusCode).build();
+    }
+    
+    @DeleteMapping("direccion/eliminar/{idDireccion}")
+    public ResponseEntity direccionDelete(@PathVariable("idDireccion") int idDireccion){
+        Result result = direccionJpaDaoImplementation.delete(idDireccion);
+        return ResponseEntity.status(result.StatusCode).build();
+    }
+    
+    @GetMapping("direccion/pais")
+    public ResponseEntity paisGetAll(){
+        Result result = paisJpaDaoImplementation.getAll();
+        return ResponseEntity.status(result.StatusCode).body(result.Object);
+    }
+    
+
+}
+ 
